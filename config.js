cce74e1c1b23551f3ccf2609c791838d4f7b4e14
Lilian Ricaud <contact@lilianricaud.com>
2016-08-25 17:55:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var GH = {}
 GH.SECRET = 'YWU0ZmRkZGFjOTVlZGM1ZTc1MmI3NjRjZTI3Y2UxZGYyMzdmZTdkZg=='
 GH.ID = 'ODUzMjY1YjA5YjBjMjVlOTg2MTQ='
 // Github Owner's
-GH.OWNER = 'multibao'
+GH.OWNER = 'lilian'
 // Github crew's repository
-GH.CREW = 'organisations'
+GH.CREW = 'PatternResearch'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'Partager et enrichir vos pratiques d\'équipe'
-MULTIBAO.UVP2 = 'Let\'s Do It Together'
-MULTIBAO.BUTTON1 = 'Découvrir'
-MULTIBAO.BUTTON2 = 'Espace contributeurs / bloggeurs'
+MULTIBAO.UVP1 = 'Shareable encyclopedia of patterns, pattern languages and sequences for generative design.'
+MULTIBAO.UVP2 = 'Open research on building living generative systems' 
+MULTIBAO.BUTTON1 = 'Pattern Languages'
+MULTIBAO.BUTTON2 = 'Pattern Research'
