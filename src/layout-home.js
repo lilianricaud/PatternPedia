35d4e539f0b2586f39c2f1654e1f0c3a54335426
Lilian Ricaud <contact@lilianricaud.com>
2016-08-25 21:51:14 +0200

    Update layout-home.js

--- src/layout-home.js
+++ src/layout-home.js
@@ -5,8 +5,8 @@
     <section class="home-intro">
         <div class="home-intro-content container">
           <h2>${MULTIBAO.UVP1}<span>${MULTIBAO.UVP2}</span></h2>
-          <a href="#multibao/contributions/blob/master/pages/commencer_ici.md">${MULTIBAO.BUTTON1}</a>
-          <a href="#multibao/documentation/blob/master/README.md">${MULTIBAO.BUTTON2}</a>
+          <a href="#lilianricaud/patterns/blob/master/README.md">${MULTIBAO.BUTTON1}</a>
+          <a href="#lilianricaud/open-social-innovation/blob/master/what-is-open-social-innovation.md">${MULTIBAO.BUTTON2}</a>
         </div>
     </section>
     <section id="gh-crew-list" class="container">
