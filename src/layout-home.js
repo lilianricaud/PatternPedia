66ea68ee3afcebf00c315b2b5680e592177394a3
Lilian Ricaud <contact@lilianricaud.com>
2016-08-25 22:01:02 +0200

    Update layout-home.js

--- src/layout-home.js
+++ src/layout-home.js
@@ -6,7 +6,7 @@
         <div class="home-intro-content container">
           <h2>S'inspirer des autres cultures<span>Rédiger et partager vos expériences</span></h2>
           <a href="#multibao/contributions/blob/master/pages/commencer_ici.md">Commencer ici</a>
-          <a href="#multibao/documentation/blob/master/README.md">Guide d'utilisation</a>
+          <a href="#lilianricaud/open-social-innovation/blob/master/what-is-open-social-innovation.md">About Open Social Innovation</a>
         </div>
     </section>
     <section id="gh-crew-list" class="container">
